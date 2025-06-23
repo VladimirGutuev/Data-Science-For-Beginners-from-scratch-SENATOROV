758d273bcdac1ddf7130467bbe8ec74eb755b26c
Vladimir Gutuev <vgutuev@gmail.com>
2025-06-23 18:17:41 +0300

    fix: correct rounding error in function

---
+++ python/commits.py
@@ -0,0 +1,8 @@
+"""This notebook is a hint for conventional commits."""
+
+# 2. Представьте, что вы исправили баг в функции, которая некорректно
+# округляет числа. Сделайте фиктивный коммит и напишите для него сообщение в
+# соответствии с Conventional Commits (используя тип fix).
+# ```bash
+# git commit -m 'fix: correct rounding error in function'
+# ```
